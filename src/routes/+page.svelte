11d175bcf744c49245cba9610988881df012cbef
eugene.chuvyrov <eugene.chuvyrov@servicenow.com>
2023-01-12 23:11:55 -0800

    Added video

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,7 +7,7 @@
 		<img src={ta_logo} alt="Tactical Athletics" style="width:256px;" />
 	</h1>
 	<center>
-		<iframe width="560" height="315" src="https://www.youtube.com/embed/zfUDzQNtBnE" title="YouTube video player" frameborder="1" allow="autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+		<iframe width="420" height="236" src="https://www.youtube.com/embed/zfUDzQNtBnE" title="YouTube video player" frameborder="1" allow="autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
 	</center>
 
 </section>
